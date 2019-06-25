86e8b07f8cd0ace81bdf7f851c08117678772a03
Harshit Pal <harshitp12345@gmail.com>
2019-06-25 16:08:33 +0530

    Day 7 continued

--- Day7/LoginApp/app/src/main/java/com/example/loginapp/MainActivity.java
+++ Day7/LoginApp/app/src/main/java/com/example/loginapp/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.loginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText username, password;
+    Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.editTextUsername);
+        password = findViewById(R.id.editTextPassword);
+        btnSubmit = findViewById(R.id.buttonSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usn = username.getText().toString();
+                String pwd = password.getText().toString();
+                if(usn.equals("Admin") && pwd.equals("admin@123")){
+                    Toast.makeText(MainActivity.this, "Authenticated Successfully", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Bhkk, galat baat", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
